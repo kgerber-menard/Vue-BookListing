cd9632043f4c99299df37222068c2c31658a26ee
Kevin Gerber <kgerber@menard-inc.com>
2020-07-10 13:32:56 -0500

    8th commit

---
+++ src/components/BookForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="" action="#" method="post">
+    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <button type="submit" name="button">Add Book</button>
+    @submit=bookSubmit(bookTitle, bookAuthor);
+  </form>
+</template>
+<script>
+  export default {
+    name: 'BookForm',
+    props: ['books'],
+    data() {
+      return{
+        bookTitle: '',
+        bookAuthor: ''
+      }
+    },
+    methods: {
+      bookSubmit(bootTitle, bookAuthor){
+        this.$emit('addBook', bootTitle, bookAuthor);
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
